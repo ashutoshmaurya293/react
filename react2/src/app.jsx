aff5cf47eaad28ec36402abefdd68b09b1c643f7
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-09-06 19:23:52 +0530

    react

---
+++ react2/src/app.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
