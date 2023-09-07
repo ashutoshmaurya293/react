9e482c8fe07ce11edb8b4925adab5fc98605a8b2
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-09-07 14:43:15 +0530

    color button

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -4,12 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("red")
 
   return (
-    <>
-     
-    </>
+  <div className='div'style={{backgroundColor:color}}>
+<button className='a' onClick={ ()=> setColor("red")}>
+  Red
+</button>
+<button className='b' onClick={ ()=> setColor("black")}>
+  Black
+</button>
+<button className='c' onClick={ ()=> setColor("yellow")}>
+  Yellow
+</button>
+<button className='d' onClick={ ()=> setColor("orange")}>
+  Orange
+</button>
+<button className='e' onClick={ ()=> setColor("green")}>
+  Green
+</button>
+<button className='f' onClick={ ()=> setColor("pink")}>
+  Pink
+</button>
+<button className='g' onClick={ ()=> setColor("blue")}>
+  Blue
+</button>
+  </div>
   )
 }
 
