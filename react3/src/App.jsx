d510ae11b9b6d8f4ccc870782776562c8d7406d8
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-09-10 21:07:49 +0530

    react3

--- react3/src/App.jsx
+++ react3/src/App.jsx
@@ -1,49 +1,82 @@
-import { useCallback, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useCallback, useState, useEffect, useRef} from "react";
+
 
 function App() {
   const [length, setlength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowrd, setCharAllowed] = useState(false);
-  const [pasward, setPasward] = useState("");
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setpassword] = useState("");
+
+  const passwordref = useRef(null)
 
   const paswardGenerater = useCallback(() => {
     let pas = "";
     let string = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
     if (numberAllowed) string += "0123456789";
-    if (charAllowrd) string += "!@#$%^&*()";
-    for (let i = 1; i <= Array.length; i++) {
+    if (charAllowed) string += "!@#$%^&*()";
+    for (let i = 1; i <= length; i++) {
       let Char = Math.floor(Math.random() * string.length + 1);
-      pas = string.charAt(Char);
-      setPasward(Char)
+      pas += string.charAt(Char);
     }
-  }, [length, numberAllowed, charAllowrd, setPasward]);
-
+    setpassword(pas)
+  }, [length, numberAllowed, charAllowed]);
+useEffect(()=>{paswardGenerater()}, [length, numberAllowed, charAllowed, paswardGenerater])
   return (
-        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-        <h1 className="text-white text-center my-3">Password generator</h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={pasward}
-            className="outline-none w-full py-1 px-3"
-            placeholder="Password"
-            readOnly
-          />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
-        </div>
-        <div className="flex text-sm gap-x-2"></div>
-        <div className="flex items-center gap-x-1"></div>
-        <input type="range"
-        min={6}
-        max={100}
-        value={length}
-        className="cursor-pointer"
-         />
-         <label> Length:{length}</label>
-
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input 
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={}
+        />
+        <button
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+        >
+          copy
+        </button>
       </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setlength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
   );
 }
 
