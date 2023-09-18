2e54f5c6c56cc687a6db1ede57239f1a26b3ea2e
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-09-18 20:34:33 +0530

    react5

---
+++ react5/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* <h1 className='bg-green-700 p-4 text-center m-8'>heact router</h1> */}
+    </>
+  )
+}
+
+export default App
