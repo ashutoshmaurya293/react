446ac5ecbd8188cd4b9a4d3968ca275255a97db4
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-09-19 21:26:55 +0530

    react5

--- react5/src/components/footer/Footer.jsx
+++ react5/src/components/footer/Footer.jsx
@@ -39,7 +39,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.comS"
+                    href="https://github.com"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
