1d3d62c08ff217d70e45d59443321498980ebd7e
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-10-29 13:06:24 +0530

    adding some style

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
-import TodoApp from './Components/TodoApp'
-
+import "./App.css";
+import TodoApp from "./Components/TodoApp";
 function App() {
-
   return (
     <>
-<TodoApp/>
+      <TodoApp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- demo/src/Components/TodoApp.jsx
+++ demo/src/Components/TodoApp.jsx
@@ -1,57 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-  const addTodo = () => {
+  const [input, setInput] = useState("");
+  const addTodo = (e) => {
+    e.preventDefault();
     if (input) {
       setTodos([...todos, { text: input, isCompleted: false }]);
-      setInput('');
+      setInput("");
     }
   };
-
   const toggleTodo = (index) => {
     const newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
   };
-  const Delete=(index)=>{
+  const Delete = (index) => {
     console.log(index);
-    const t = [...todos]
-    t.splice(index,1)
-    setTodos(t)
-  }
-
+    const t = [...todos];
+    t.splice(index, 1);
+    setTodos(t);
+  };
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Enter a Todo"
-      />
-      <button onClick={addTodo}>Add Todo</button>
-      <div style={{ marginTop: '20px' }}>
+    <>
+      <form class="flex items-center justify-center mt-10">
+        <input
+          className="border-2 border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200 rounded-md p-2"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter a Todo"
+        />
+        <button
+          className="ml-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={addTodo}
+        >
+          Add Todo
+        </button>
+      </form>
+      <div className="flex items-center justify-center mt-10 flex-col">
         {todos.map((todo, index) => (
           <div
+            className="flex  p-1.5"
             key={index}
             style={{
-              textDecoration: todo.isCompleted ? 'line-through' : 'none',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: '10px',
+              backgroundColor: todo.isCompleted ? "lightgreen" : "white",
             }}
           >
-            <div style={{ marginRight: '10px' }}>{todo.text}</div>
-            <button onClick={()=>Delete(index)}>Delete</button>
-            <button onClick={() => toggleTodo(index)}>
-              {todo.isCompleted ? 'Undo' : 'Complete'}
+            <div className="w-72 pl-3 pt-1 border-2 border-gray-300">
+              {todo.text}
+            </div>
+            <button
+              className="ml-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => Delete(index)}
+            >
+              Delete
+            </button>
+            <button
+              className="ml-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => toggleTodo(index)}
+            >
+              {todo.isCompleted ? "Undo" : "Complete"}
             </button>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
