5b578429b8aec54bd00127d52c7b513a8ce4c992
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-08 20:12:29 +0530

    card project

---
+++ card-project/src/App.jsx
@@ -0,0 +1,16 @@
+import react, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+const [card, setcard] = useState()
+
+  return (
+    <>
+     {card}
+    </>
+  )
+}
+
+export default App
