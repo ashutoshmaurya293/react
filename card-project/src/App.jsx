e3767b5ef35fcadc06df8be7261121d005e3b392
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-09 19:35:15 +0530

    adding font-family

--- card-project/src/App.jsx
+++ card-project/src/App.jsx
@@ -1,16 +1,13 @@
-import react, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-const [card, setcard] = useState()
-
   return (
     <>
-     {card}
+      <div className="app">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quibusdam nemo excepturi et iure ratione inventore sapiente quidem, amet quam nostrum quae recusandae rerum corrupti distinctio, qui labore architecto atque.</div>
+      <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti debitis recusandae dolore aspernatur sequi ex possimus sed expedita veniam qui vero eius obcaecati quae reprehenderit nobis, natus accusamus eum. At?</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
