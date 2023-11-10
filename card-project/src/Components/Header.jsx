fdbc2c486b549256e1835f11ce4ab1a0066bdd84
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-10 15:03:33 +0530

    making header navbar

--- card-project/src/Components/Header.jsx
+++ card-project/src/Components/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import { IoReorderThreeSharp } from "react-icons/io5";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <nav>
+      <div className="first"><h1>Logo</h1></div>
+      <div className="second">
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>Contact</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div className="third">
+<p>cart</p>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
