0162845f06a4c89d6170652294aa419f2cdfe24f
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-14 13:12:13 +0530

    making cart section

--- card-project/src/Components/Cart.jsx
+++ card-project/src/Components/Cart.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import "./Cart.css";
 import { AiFillDelete } from "react-icons/ai";
-const img1 ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZPbw47gPMdhi7FcnnZkzlhk07Y15td8TdioCwPv6nKfLNm0eATnTmZYqnWC1-6rvZB0&usqp=CAU",
+const img1 =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZPbw47gPMdhi7FcnnZkzlhk07Y15td8TdioCwPv6nKfLNm0eATnTmZYqnWC1-6rvZB0&usqp=CAU";
 
 function Cart() {
-
   return (
     <div className="cart">
       <main>
-        <CartItem 
+        <CartItem
           imgsrc={img1}
-          name={macbook}
+          name={"macbook"}
           price={100000}
           qty={1}
-          id={
-            "asdfaf"
-          }
-        
+          id={"asdfaf"}
         />
       </main>
-      <aside>
-        <h2>Subtotal:${2000}</h2>
+      <aside className="total-wala">
+        <h2 className="sub">Subtotal:${2000}</h2>
         <h2>shiping:${200}</h2>
         <h2>Tax:${11}</h2>
         <h2 className="total">Total:${25000}</h2>
@@ -42,14 +39,14 @@ const CartItem = ({
     <img src={imgsrc} alt="image" />
     <article>
       <h3>{name}</h3>
-      <p>4{price}</p>
+      <p>{price}</p>
     </article>
-    <div>
+    <div className="button-section">
       <button onClick={() => decrement(id)}>-</button>
       <p>{qty}</p>
       <button onClick={() => increment(id)}>+</button>
     </div>
-    <AiFillDelete onClick={() => deleteHandler(id)} />
+    <AiFillDelete onClick={() => deleteHandler(id)} className="delete-button" />
   </div>
 );
 
