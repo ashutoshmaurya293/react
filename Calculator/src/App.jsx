cfc0bf912a3bf01a5cb98c40ace696e35cb12247
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-05 19:18:59 +0530

    adding some styling in Calculator

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -5,7 +5,7 @@ import Calculator from "./components/Calculator";
 const App = () => {
   return (
     <div>
-   <h1 className="heading">   Calculator</h1>
+   {/* <h1 className="heading">   Calculator</h1> */}
       <Calculator />
     </div>
   );
