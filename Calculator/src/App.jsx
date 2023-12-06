1e6c13c89bad016962b264a028954f03eaaa63a0
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-06 19:20:48 +0530

    calculator completed

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import './index.css'
 import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <div>
-   {/* <h1 className="heading">   Calculator</h1> */}
       <Calculator />
     </div>
   );
---
+++ Calculator/src/components/CalculatorButton.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Calculator.css";
+
+const CalculatorButton = ({
+  previous,
+  current,
+  simble,
+  allclearHandler,
+  deleteHandler,
+  chooseOperationHandler,
+  appendValueHandler,
+  equalHandler,
+}) => {
+  return (
+    <>
+      <div className="Container">
+        <div className="screen">
+          <div className="Prevoius">
+            {previous}
+            {simble}
+          </div>
+          <div className="current">{current}</div>
+        </div>
+        <button className="button" id="width" onClick={allclearHandler}>
+          AC
+        </button>
+        <button className="button" onClick={deleteHandler}>
+          DEL
+        </button>
+        <button data={"÷"} onClick={chooseOperationHandler}>
+          ÷
+        </button>
+        <button className="button" data={7} onClick={appendValueHandler}>
+          7
+        </button>
+        <button className="button" data={8} onClick={appendValueHandler}>
+          8
+        </button>
+        <button className="button" data={9} onClick={appendValueHandler}>
+          {" "}
+          9
+        </button>
+        <button className="button" data={"x"} onClick={chooseOperationHandler}>
+          x
+        </button>
+        <button className="button" data={4} onClick={appendValueHandler}>
+          4
+        </button>
+        <button className="button" data={5} onClick={appendValueHandler}>
+          5
+        </button>
+        <button className="button" data={6} onClick={appendValueHandler}>
+          6
+        </button>
+        <button className="button" data={"+"} onClick={chooseOperationHandler}>
+          +
+        </button>
+        <button className="button" data={1} onClick={appendValueHandler}>
+          1
+        </button>
+        <button className="button" data={2} onClick={appendValueHandler}>
+          2
+        </button>
+        <button className="button" data={3} onClick={appendValueHandler}>
+          3
+        </button>
+        <button className="button" data={"-"} onClick={chooseOperationHandler}>
+          -
+        </button>
+        <button className="button" data={"."} onClick={appendValueHandler}>
+          .
+        </button>
+        <button className="button" data={0} onClick={appendValueHandler}>
+          0
+        </button>
+        <button id="last" className="button" onClick={equalHandler}>
+          =
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CalculatorButton;
