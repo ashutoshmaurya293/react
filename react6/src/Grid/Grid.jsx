35e4dce052db29ac30d37ce6b54fad387df4f3ee
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-01-28 18:55:24 +0530

    bug fixed

--- react6/src/Grid/Grid.jsx
+++ react6/src/Grid/Grid.jsx
@@ -7,25 +7,29 @@ function Grid({ numberOfCards }) {
   const [board, setBoard] = useState(Array(numberOfCards).fill(""));
   const [winner, setWinner] = useState(null);
   const [turn, setTurn] = useState(true);
+ 
   function play(index) {
-    if (board == true) {
-      board[index] = "O";
-    } else {
-      board[index] = "X";
-    }
-    const win = CheakWinner[(board, turn ? "O" : "X")];
+  if (board[index] === "") {
+    board[index] = turn ? "O" : "X"; // Place the current player's symbol
+    const win = CheakWinner(board, turn ? "O" : "X"); // Check for a winner
     if (win) {
       setWinner(win);
     }
-    setBoard([...board]);
-    setBoard(!turn);
+    console.log(board);
+    setBoard([...board]); // Update the board state
+    setTurn(!turn); // Toggle the turn
   }
+}
+const reset =()=>{
+  setBoard(Array(numberOfCards).fill(""))
+  setWinner(false)
+}
   return (
     <div className="grid-wrapper">
       {winner && (
         <>
           <h1 className="turn-highlight">Winner is {winner}</h1>
-          <button>Reset</button>
+          <button onClick={reset}>Reset</button>
         </>
       )}
       <h1 className="turn-highlight">Current turn :{turn ? "O" : "X"}</h1>
